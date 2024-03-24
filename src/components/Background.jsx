4fe80b2e9f29e9986e2467b6975f277e34ba7cbf
“aakriti <aakritikhanna206@gmail.com>
2024-03-24 17:19:20 +0530

    docs mini

---
+++ src/components/Background.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Background(){
+  return (
+    <>
+    <div className='absolute top-[5%] w-full py-10  flex justify-center  text zinc-900 text-xl  font-semibold'>Documents</div>
+    <h1 className='text-[13vw] leading-none tracking-tighter absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] font-semibold text-zinc-1000'>Docs.</h1>
+    </>
+
+  )
+}
+
+export default Background
